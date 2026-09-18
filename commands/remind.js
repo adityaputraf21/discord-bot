433dd2c06ee6d91af90ad4634bb7102cd5b76cd2
Developer <dev@example.com>
2026-09-19 02:35:12 +0800

    Remove unused natural module

--- commands/remind.js
+++ commands/remind.js
@@ -1,5 +1,3 @@
-const natural = require('natural');
-
 function parseReminderDate(input) {
   // Format: "YYYY-MM-DD HH:mm" atau natural language
   const isoRegex = /(\d{4})-(\d{2})-(\d{2})\s+(\d{2}):(\d{2})/;
